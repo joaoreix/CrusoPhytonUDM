c9d37feb2f295d4ffdeed7f1fa2f73560bfcb4a0
João Reis <joao.reis@selenium.ao>
2025-08-27 15:50:55 +0100

    Minha mensagem de alteração

---
+++ variaveis.py
@@ -0,0 +1,15 @@
+#tipos de variaveis
+#str - String (textos)
+#int - Numeros inteiros
+#float - numeros reais
+#bool - Lógico (verdadeiro/falso)
+
+nome='Wagner cardoso'
+idadeCliente=41
+profissao= 'Analista de sistemas'
+salario= 4000.00
+print(f'Hi, {nome}')
+print('Idade: ', idadeCliente)
+print('A profissão é: ', profissao)
+print('O salario é: ', salario, 'akzs')
+#print('nome do cliente: ', nome  'a idade: ' + idadeCliente + ' a profissão é ' + profissao  +'e o salario é ' + salario +'akzs')
