6627952c3ff558a184b1deff875a3d3727ed0187
joao.reix <carlosreix1.cr@gmail.com>
2025-08-27 16:04:37 +0100

    git log --oneline

---
+++ calculos.py
@@ -0,0 +1,11 @@
+nomeComprador = 'Theo Augusto'
+produto = 'Celular'
+precoCell = 1500.00
+quantidade = 2  
+
+print('O cliente: ', nomeComprador)
+print('Comprou: ', quantidade)
+print('unidades do produto: ', produto)    
+print('Cada um custa: ', precoCell, 'akzs')
+total = precoCell * quantidade
+print('O total da compra é: ', total, 'akzs')
